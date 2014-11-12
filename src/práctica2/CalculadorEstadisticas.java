a3183b2d28ba770adc448a173530b44fe5191f91
usuario <usuario@10.22.143.9>
2014-11-12 13:28:27 +0000

    Ordena2Vector está tope guay

---
+++ src/práctica2/CalculadorEstadisticas.java
@@ -0,0 +1,6 @@
+public class CalculadorEstadisticas {
+    
+    public static DatosEstadisticos estadisticaOrdenaVector(Ordenar1Vector ordenaVector, int size, boolean orden) {
+        
+    }    
+}
