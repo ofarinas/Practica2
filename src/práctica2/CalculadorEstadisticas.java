b55062e33eeab4571be24ba2877b82dc41951333
usuario <usuario@10.22.143.10>
2014-11-12 13:50:56 +0000

    sekxiiiii

--- src/práctica2/CalculadorEstadisticas.java
+++ src/práctica2/CalculadorEstadisticas.java
@@ -1,6 +1,25 @@
+package práctica2;
+
 public class CalculadorEstadisticas {
     
     public static DatosEstadisticos estadisticaOrdenaVector(Ordenar1Vector ordenaVector, int size, boolean orden) {
-        
-    }    
+        DatosEstadisticos resultado = new DatosEstadisticos();
+        return resultado;
+    }
+    
+    public static MediaDatosEstadisticos estadisticaOrdenaVectorAleatorio(OrdenarVector m, int tam_vector){
+        MediaDatosEstadisticos resultado = new MediaDatosEstadisticos();
+        return resultado;
+    }
+    
+    public static DatosEstadisticos[] estadisticasOrdenaVectoresInversos(OrdenarVector m, int[] tam_vectores){
+        DatosEstadisticos[] resultados = new DatosEstadisticos[tam_vectores.length];
+        return resultados;
+    }
+    
+    public static MediaDatosEstadisticos[] estadisticasOrdenaVectoresAleatorios(OrdenarVector m, int[] tam_vectores){
+        MediaDatosEstadisticos[] resultados = new MediaDatosEstadisticos[tam_vectores.length];
+        return resultados;
+    }
+       
 }
